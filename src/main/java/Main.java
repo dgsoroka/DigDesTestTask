b6789b29358842246d06a5ac066889438d1b9211
Soroka Dmitriy <dima.soroka.2014@mail.ru>
2023-03-25 16:27:38 +1000

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import com.digdes.school.JavaSchoolStarter;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JavaSchoolStarter starter = new JavaSchoolStarter();
+    }
+}
---
+++ src/main/java/com/digdes/school/JavaSchoolStarter.java
@@ -0,0 +1,14 @@
+package com.digdes.school;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class JavaSchoolStarter {
+
+    private List<Map<String, Object>> table;
+
+    public JavaSchoolStarter(){
+        this.table = new ArrayList<Map<String, Object>>();
+    }
+}
